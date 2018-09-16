5f1dac0e52eed16828baf5a50b74bee02610cc7a
ben-woolnough <ben.woolnough76@hotmail.com>
2018-09-16 15:09:39 +0100

    Removed image optimization task

--- gulpfile.js
+++ gulpfile.js
@@ -5,15 +5,10 @@ var useref = require('gulp-useref');
 var uglify = require('gulp-uglify');
 var gulpIf = require('gulp-if');
 var cssnano = require('gulp-cssnano');
-var imagemin = require('gulp-imagemin');
 var cache = require('gulp-cache');
 var del = require('del');
 var runSequence = require('run-sequence');
 
-// Basic Gulp task syntax
-gulp.task('hello', function() {
-  console.log('Hello Zell!');
-})
 
 // Development Tasks 
 // -----------------
@@ -25,7 +20,7 @@ gulp.task('browserSync', function() {
       baseDir: 'app'
     }
   })
-})
+});
 
 gulp.task('sass', function() {
   return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
@@ -34,14 +29,14 @@ gulp.task('sass', function() {
     .pipe(browserSync.reload({ // Reloading with Browser Sync
       stream: true
     }));
-})
+});
 
 // Watchers
 gulp.task('watch', function() {
   gulp.watch('app/scss/**/*.scss', ['sass']);
   gulp.watch('app/*.html', browserSync.reload);
   gulp.watch('app/js/**/*.js', browserSync.reload);
-})
+});
 
 // Optimization Tasks 
 // ------------------
@@ -56,31 +51,21 @@ gulp.task('useref', function() {
     .pipe(gulp.dest('dist'));
 });
 
-// Optimizing Images 
-gulp.task('images', function() {
-  return gulp.src('app/images/**/*.+(png|jpg|jpeg|gif|svg)')
-    // Caching images that ran through imagemin
-    .pipe(cache(imagemin({
-      interlaced: true,
-    })))
-    .pipe(gulp.dest('dist/images'))
-});
-
 // Copying fonts 
 gulp.task('fonts', function() {
   return gulp.src('app/fonts/**/*')
     .pipe(gulp.dest('dist/fonts'))
-})
+});
 
 // Cleaning 
 gulp.task('clean', function() {
   return del.sync('dist').then(function(cb) {
     return cache.clearAll(cb);
   });
-})
+});
 
 gulp.task('clean:dist', function() {
-  return del.sync(['dist/**/*', '!dist/images', '!dist/images/**/*']);
+  return del.sync(['dist/**/*']);
 });
 
 // Build Sequences
@@ -90,13 +75,13 @@ gulp.task('default', function(callback) {
   runSequence(['sass', 'browserSync'], 'watch',
     callback
   )
-})
+});
 
 gulp.task('build', function(callback) {
   runSequence(
     'clean:dist',
     'sass',
-    ['useref', 'images', 'fonts'],
+    ['useref', 'fonts'],
     callback
   )
-})
+});
